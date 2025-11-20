d6187107cc72f2cf769f7e112f980eda49d03f25
angelinawwu <angelinawu05@gmail.com>
2025-11-19 22:53:42 -0800

    dynamic update

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -2,6 +2,8 @@ import { getPoems } from '../actions'
 import { Sidebar } from '@/components/Sidebar'
 import { MobileNav } from '@/components/MobileNav'
 
+export const dynamic = 'force-dynamic'
+
 export default async function PublicLayout({
   children,
 }: {
